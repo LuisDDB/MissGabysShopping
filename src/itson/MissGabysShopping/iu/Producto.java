44f5ef371faf4b7dafe67c47114d91314e4ddac3
luisd <luisd@Kirby>
2024-04-23 08:14:17 -0700

    Se unio la rama ProductoYVenta con la master

--- src/itson/MissGabysShopping/iu/Producto.java
+++ src/itson/MissGabysShopping/iu/Producto.java
@@ -49,6 +49,7 @@ public class Producto extends javax.swing.JFrame {
         btnEliminarP = new javax.swing.JButton();
         txfCampo = new javax.swing.JTextField();
         jPanel1 = new javax.swing.JPanel();
+        jLabel10 = new javax.swing.JLabel();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
 
@@ -182,15 +183,23 @@ public class Producto extends javax.swing.JFrame {
                 .addContainerGap())
         );
 
+        jLabel10.setText("Buscar producto");
+
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
         jPanel1Layout.setHorizontalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 512, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addGap(214, 214, 214)
+                .addComponent(jLabel10)
+                .addContainerGap(212, Short.MAX_VALUE))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 0, Short.MAX_VALUE)
+            .addGroup(jPanel1Layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(jLabel10)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout jPanelMainLayout = new javax.swing.GroupLayout(jPanelMain);
@@ -281,6 +290,7 @@ public class Producto extends javax.swing.JFrame {
     private javax.swing.JButton btnIngresarP;
     private javax.swing.JComboBox<String> cbxCampo;
     private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel10;
     private javax.swing.JLabel jLabel2;
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
